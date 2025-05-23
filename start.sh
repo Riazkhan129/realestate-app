bbf395130c06bbaf9c60ab92f40e90f44deb98f9
riaz <riazkhankse@gmail.com>
2025-05-23 21:07:26 +0500

    change streamlit port to 8501

--- start.sh
+++ start.sh
@@ -4,4 +4,5 @@
 uvicorn main:app --host 0.0.0.0 --port 8000 &
 
 # Start Streamlit on the Railway public port
-streamlit run streamlit_app.py --server.address=0.0.0.0 --server.port=$PORT
+streamlit run streamlit_app.py --server.address=0.0.0.0 --server.port 8501
+
